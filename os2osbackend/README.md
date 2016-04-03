ba6768ce28c96e962b7b764fc2d6593222c39fde
Sebastien Dupont <dupont.sebastien@gmail.com>
2016-04-03 03:49:34 +0200

    login/logout/register

---
+++ os2osbackend/README.md
@@ -0,0 +1,1 @@
+http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/
--- os2osbackend/app.js
+++ os2osbackend/app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -20,10 +23,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +42,15 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // error handlers
 
